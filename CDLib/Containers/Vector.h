8a29b375573f62b963e2c472215ff14f397684c4
lab8 <lab8@hq.cu-bx.com>
2020-08-23 10:48:51 +0300

    vector container

---
+++ CDLib/Containers/Vector.h
@@ -0,0 +1,105 @@
+#pragma once
+
+template<typename T>
+class Vector
+{
+public:
+	Vector()
+	{
+		re_alloc(2);
+	}
+
+	~Vector()
+	{
+		clear();
+		::operator delete(m_data, m_capacity * sizeof(T));
+	}
+
+	void push_back(const T& val)
+	{
+		if (m_size >= m_capacity)
+			re_alloc(m_capacity + m_capacity / 2 + 1);
+
+		new(&m_data[m_size++]) T(val);
+	}
+
+	void push_back(T&& val)
+	{
+		if (m_size >= m_capacity)
+			re_alloc(m_capacity + m_capacity / 2 + 1);
+
+		new (&m_data[m_size++]) T(std::move(val));
+	}
+
+	template<typename... Args>
+	T& emplace_back(Args&&... args)
+	{
+		if (m_size >= m_capacity)
+			re_alloc(m_capacity + m_capacity / 2 + 1);
+
+		new(&m_data[m_size]) T(std::forward<Args>(args) ...);
+		return m_data[m_size++];
+	}
+
+	void pop_back()
+	{
+		if (m_size > 0)
+		{
+			m_size--;
+			m_data[m_size].~T();
+		}
+	}
+
+	void clear()
+	{
+		for (size_t i = 0; i < m_size; i++)
+			m_data[i].~T();
+
+		m_size = 0;
+	}
+
+	const T& operator[](size_t index) const 
+	{
+#ifdef _DEBUG
+		if (!(index < m_size)) // vector overflow case #debugonly
+			__debugbreak();
+#endif
+
+		return m_data[index]; 
+	}
+
+	T& operator[](size_t index) 
+	{
+#ifdef _DEBUG
+		if (!(index < m_size)) // vector overflow case #debugonly
+			__debugbreak();
+#endif
+
+		return m_data[index]; 
+	}
+
+	size_t size() const { return m_size; }
+
+private:
+	void re_alloc(size_t new_capacity)
+	{
+		T* new_block = (T*)::operator new(new_capacity * sizeof(T));
+
+		if (new_capacity < m_size)
+			m_size = new_capacity;
+
+		for (size_t i = 0; i < m_size; ++i)
+			new (new_block + i) T(std::move(m_data[i]));
+
+		for (size_t i = 0; i < m_size; ++i)
+			m_data[i].~T();
+
+		::operator delete(m_data, new_capacity * sizeof(T));
+		m_data = new_block;
+		m_capacity = new_capacity;
+	}
+private:
+	size_t m_size = 0;
+	size_t m_capacity = 0;
+	T* m_data = nullptr;
+};
--- Test/Main.cpp
+++ Test/Main.cpp
@@ -1,21 +1,127 @@
 #include <iostream>
 #include <array>
+#include <vector>
 #include <string>
 #include "Algs.h"
 #include "Array.h"
+#include "Vector.h"
+
+struct Vector3
+{
+	float x = 0.0f, y = 0.0f, z = 0.0f;
+	int* m_MemoryBlock = nullptr;
+
+	Vector3(){
+		m_MemoryBlock = new int[5];
+	}
+	Vector3(float scalar)
+		:x(scalar), y(scalar), z(scalar) {
+		m_MemoryBlock = new int[5];
+	}
+	Vector3(float x, float y, float z)
+		:x(x), y(y), z(z) {
+		m_MemoryBlock = new int[5];
+	}
+
+	Vector3(const Vector3& other) = delete;
+
+	Vector3(Vector3&& other)
+		:x(other.x), y(other.y), z(other.z)
+	{
+		m_MemoryBlock = other.m_MemoryBlock;
+		other.m_MemoryBlock = nullptr;
+		std::cout << "Move\n";
+	}
+	~Vector3()
+	{
+
+		delete[] m_MemoryBlock;
+		std::cout << "Destroy\n";
+	}
+
+	Vector3& operator=(const Vector3& other) = delete;
+
+	Vector3& operator=(Vector3&& other)
+	{
+		m_MemoryBlock = other.m_MemoryBlock;
+		other.m_MemoryBlock = nullptr;
+		std::cout << "Move\n";
+		x = other.x;
+		y = other.y;
+		z = other.z;
+
+		return *this;
+	}
+};
+
+void print_vec(const Vector<Vector3>& vec)
+{
+	for (size_t i = 0; i < vec.size(); i++)
+		std::cout << vec[i].x << ", " << vec[i].y << ", " << vec[i].z << ", " << std::endl;
+}
+template<typename T>
+void print_vec(const Vector<T>& vec)
+{
+	for (size_t i = 0; i < vec.size(); i++)
+		std::cout << vec[i] << std::endl;
+}
+
+void matrix_multiply_c(float* A, float* B, float* C, unsigned int n, unsigned int m, unsigned int k) {
+	// n - A rows
+	// m - B cols
+	// k - A cols/B rows
+	for (unsigned int i_idx = 0; i_idx < n; i_idx++) {
+		for (unsigned int j_idx = 0; j_idx < m; j_idx++) {
+			float result = 0;
+			for (unsigned int k_idx = 0; k_idx < k; k_idx++) {
+				result += A[n*i_idx + k_idx] * B[m*k_idx + j_idx];
+			}
+			C[n*j_idx + i_idx] = result;
+			std::cout << result << ", ";
+		}
+		std::cout << "\n";
+	}
+}
 
 int main() {
-	int arr[5] = { -2,-3,-1,-5,-7 };
+	/*
+	Vector<std::string> vec;
+	vec.emplace_back("Chen");
+	vec.push_back("Ha");
+	vec.push_back("Meleh");
+	print_vec(vec);
+	{
 
-	std::cout << cd::sum_of_abs(arr,5) << std::endl;
+		Vector<Vector3> vec3;
+		vec3.emplace_back(4.f);
+		vec3.emplace_back(1.f, 2.f, 3.f);
+		vec3.emplace_back(2.f, 6.f, 5.f);
+		vec3.emplace_back();
+		print_vec(vec3);
+		vec3.pop_back();
+		vec3.pop_back();
+		print_vec(vec3);
+		vec3.emplace_back(3.f, 7.f, 2.f);
+		vec3.emplace_back(4.f, 8.f, 0.f);
+		print_vec(vec3);
+		vec3.clear();
+		print_vec(vec3);
+		vec3.emplace_back(3.f, 7.f, 2.f);
+		vec3.emplace_back(4.f, 8.f, 0.f);
+		print_vec(vec3);
+	}
+	*/
 
-	cd::Array<std::string, 3> arrStr;
-	arrStr[0] = "Chen";
-	arrStr[1] = "Che";
-	arrStr[2] = "Chenss";
-
-	std::cout << arrStr[cd::max_element_loc(arrStr.data(), arrStr.size())] << std::endl;
-
+	float A[16] = { 9, 5, 1, 3, 0,
+					5, 3, 6, 7, 7,
+					1, 7, 8, 1, 7};
+	float B[16] = { 6, 3, 1,
+					5, 8, 1,
+					5, 3, 3,
+					4, 7, 6,
+					8, 9, 9 };
+	float C[16];
+	matrix_multiply_c(A, B, C, 3, 3, 5);
 	std::cin.get();
 
 	return 0;
